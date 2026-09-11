80e03be507f7888588ae7d6b57986888bb87ac65
VolodAs <vladimiraseev89@gmail.com>
2026-09-11 13:00:19 +0200

    rename sendMail util

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -10,6 +10,6 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendMail = async (options) => {
+export const sendEmail = async (options) => {
   return await transporter.sendMail(options);
 };
